68b99de86a5b84dbe5db8f2c0e893ba95fe8e78d
ethandang1 <58610660+ethandang1@users.noreply.github.com>
2020-02-10 14:01:44 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+let name = game.askForString("What is your name?")
+let color = game.askForString("What is your favorite color?")
+let pet = game.askForString("Dog or Cat?")
+game.splash("Your name is: " + name + " Your favorite color is: " + color + " You are a: " + pet + " person")
